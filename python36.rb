a3a4f2c3faeb25f462d017a34b5b04db943f3037
TheRinger <TheRinger@users.noreply.github.com>
2019-01-26 12:30:28 -0800

    Update python36.rb

--- python36.rb
+++ python36.rb
@@ -3,7 +3,7 @@ class Python36 < Formula
   homepage "https://www.python.org/"
   url "www.python.org/ftp/python/3.6.5/Python-3.6.5.tgz"
   version "3.6.5"
-  sha256 "a3a92a984debd205e04f6784d0a7a1eafae04b11cdc4760d02d24cf9452a482a"
+  sha256 "53a3e17d77cd15c5230192b6a8c1e031c07cd9f34a2f089a731c6f6bd343d5c6"
 
   def install
     cflags = "CFLAGS=-I/usr/local/opt/openssl/include"
